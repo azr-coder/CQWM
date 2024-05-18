762238830a40344d79e847869c5ddde30dd56f4c
Jack An <3098461553@qq.com>
2024-05-18 18:34:53 +0800

    新增员工代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,22 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+
+
+
+    @ExceptionHandler
+    public  Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        // Duplicate entry 'zhangsan' for key 'employee.idx_username'
+        String exinfo=ex.getMessage();
+        if (exinfo.contains("Duplicate entry")){
+            String[] split = exinfo.split(" ");
+            String temp=split[2];
+            String msg=temp+ MessageConstant.ALREADY_EXIST;
+            return Result.error(msg);
+        }else {
+            return Result.error("未知错误");
+        }
+    }
+
 
 }
